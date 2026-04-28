cbca216cfbe2c9ab90332b4a2659c6d1b736f401
TeamItda <smu2026itda@gmail.com>
2026-04-28 23:16:16 +0900

    충돌 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_maps_flutter_android/google_maps_flutter_android.dart';
 import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
 
 import 'app.dart';
+import 'firebase_options.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   final mapsImplementation = GoogleMapsFlutterPlatform.instance;
   if (!kIsWeb && mapsImplementation is GoogleMapsFlutterAndroid) {
     mapsImplementation.useAndroidViewSurface = true;
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'app.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'firebase_options.dart';
+  }
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env');
-  if (Firebase.apps.isEmpty){
+
+  if (Firebase.apps.isEmpty) {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
